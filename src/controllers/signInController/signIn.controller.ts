e6efd73fda02f38b64a61973f22d1b5a8bf70e63
ramses-ayala <ramses0595@gmail.com>
2024-01-20 21:33:15 -0600

    trying to implement sign and signUp endpoints

---
+++ src/controllers/signInController/signIn.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+
+import bcrypt from 'bcrypt';
+
+import { generateToken } from '../../utils/generateToken';
+
+
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const signIn = async (req: Request, res: Response) => {
+    
+    const { email, password } = req.body;
+
+    try {
+
+        const user = await prisma.users.findUnique({
+            where: {
+                email
+            }
+        });
+
+        if(!user) return res.status(404).json({'data': 'User not found !!!'});
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if(match !== true){
+            return res.status(404).json({'data': 'Password incorrect !!!'});
+        }
+
+        const token = generateToken(user);
+
+        return res.status(200).json({'data': {'email': user.email, 'token': token}});
+
+    } catch (error) {
+        res.status(500).json({"error": error});
+    }
+}
+
+export {signIn};
--- src/controllers/signUpController/signUp.controller.ts
+++ src/controllers/signUpController/signUp.controller.ts
@@ -1,12 +1,40 @@
 import { Request, Response } from "express";
 
-const signUp = (_req: Request, res: Response) => {
+import bcrypt from "bcrypt";
 
-    //const {email, password} = req.body;
+import { credentials } from "../../types/credentials/credentials";
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const signUp = async(req: Request<{}, {}, credentials>, res: Response) => {
+
+    const {email, password} = req.body;
 
     try {
-        
-        res.status(200).json({"msg": "signUp"});
+        const userFound = await prisma.users.findUnique({
+            where: {
+                email
+            }
+        });
+
+        if(userFound){
+            return res.status(200).json({'msg': 'That email is already registered !!!'});
+        }
+
+        const encryptedPassword = bcrypt.hashSync(password, 10);
+
+        const userData = {
+            ...req.body,
+            password: encryptedPassword
+        };
+
+        await prisma.users.create({
+            data: userData,
+        });
+
+        res.status(200).json({"data": "user registered"});
+
     } catch (error) {
         console.error("Occurs an error signing up the user : ", error);
         res.status(500).json({"error": error});
